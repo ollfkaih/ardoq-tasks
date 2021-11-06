2ea5a2d274f42eb00980aa3c8700feb5201c0a24
Olav Kihle <olavki@stud.ntnu.no>
2021-11-06 15:04:56 +0100

    Use api to get all stations
    Now fetches json from api and shows total parking spaces in tooltip.

--- oslo-bysykkel-kart/src/App.js
+++ oslo-bysykkel-kart/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Map from './Map';
-import './App.css'
+import React from "react";
+import Map from "./components/Map";
 
 const App = () => {
-    return (
-        <div>
+  return (
+    <div style={{alignContent: 'center'}}>
+        <a href="/" style={{textDecoration: 'none', color: 'black'}}>
+            <h1 style={{fontFamily: 'monospace'}}>Oslo bysykkel - available bikes and parking</h1>
+        </a>
         <Map/>
-        </div>
-        );
-    }
-    
+    </div>
+  );
+};
+
 export default App;
---
+++ oslo-bysykkel-kart/src/components/Map.jsx
@@ -0,0 +1,123 @@
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import "./Map.css";
+import ReactDOM from "react-dom";
+import Tooltip from "./Tooltip";
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoib2xsZmthaWgiLCJhIjoiY2t2Y25oZW85MDdhZTJubjNxOWE5eWo4aiJ9.yZfjmSUM0p6A_bLoXAJR9g";
+
+const Map = () => {
+  const client_identifier = "kihle-hobbyprosjekt";
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const lng = 10.75;
+  const lat = 59.92;
+  const zoom = 11;
+  const popUpRef = useRef(new mapboxgl.Popup({ offset: 15 }));
+  const [stations, setStations] = useState();
+  const [stationsStatus, setStationsStatus] = useState();
+
+  const fetchStations = async (url, setter) => {
+    return fetch(url, {
+      headers: {
+        "Client-Identifier": { client_identifier },
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => setter(json.data.stations));
+  };
+
+  useEffect(() => {
+    fetchStations(
+      "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json",
+      setStations
+    );
+    fetchStations(
+      "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json",
+      setStationsStatus
+    );
+  }, []);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    if (!stations || !stationsStatus) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    map.current.on("click", (e) => {
+      const features = map.current.queryRenderedFeatures(e.point, {
+        layers: ["stationsMapLayer"],
+      });
+      if (features.length) {
+        const feature = features[0];
+        const currentStationStatus = stationsStatus.find(
+          (station) => station.station_id === feature.properties.station_id
+        );
+        const currentStationInfo = stations.find(
+          (s) => s.station_id === feature.properties.station_id
+        );
+        const popupNode = document.createElement("div");
+        ReactDOM.render(
+          <Tooltip
+            station_name={currentStationInfo.name}
+            num_bikes_available={currentStationStatus.num_bikes_available}
+            num_docks_available={currentStationStatus.num_docks_available}
+            capacity={currentStationInfo.capacity}
+          />,
+          popupNode
+        );
+        popUpRef.current
+          .setLngLat(e.lngLat)
+          .setDOMContent(popupNode)
+          .addTo(map.current);
+      }
+    });
+  }, [stations, stationsStatus]);
+
+  useEffect(() => {
+    if (!map.current) return;
+    if (!stations || !stationsStatus) return;
+
+    const stationsGeoJSONArray = stations?.map((station) => {
+      return {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [station.lon, station.lat],
+        },
+        properties: { station_id: station.station_id },
+      };
+    });
+
+    const featureCollection = {
+      type: "FeatureCollection",
+      features: stationsGeoJSONArray,
+    };
+
+    map.current.on("load", () => {
+      map.current.addSource("stationsMapLayer", {
+        type: "geojson",
+        data: featureCollection,
+      });
+      map.current.addLayer({
+        id: "stationsMapLayer",
+        type: "circle",
+        source: "stationsMapLayer",
+        paint: {
+          "circle-color": "#11aa22",
+          "circle-radius": 8,
+          "circle-stroke-color": "#222222",
+          "circle-stroke-width": 2,
+        },
+      });
+    });
+  }, [stations, stationsStatus]);
+
+  return <div ref={mapContainer} className="map-container"></div>;
+};
+export default Map;
